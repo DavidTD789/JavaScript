975dee55307403b5cc69671e60fb37d32dc49b22
DavidTD789 <138784445+DavidTD789@users.noreply.github.com>
2023-07-23 14:24:47 +0400

    JavaScript
    JS

---
+++ password.js
@@ -0,0 +1,10 @@
+let password = 'qwgdhjgjhgjghjghjgiop'
+
+if(password.length < 6 || password.length > 10){
+    console.log('Its')
+    console.log('a')
+    console.log('false')
+    console.log('password')
+}else {
+    console.log('true')
+}
